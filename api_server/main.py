66d0fb9ee00939ae0be113fe02e910f0f036148b
namansehwal <namansehwal@gmail.com>
2024-04-03 03:30:21 +0530

    Update dependencies and add git logger

--- api_server/main.py
+++ api_server/main.py
@@ -1,21 +1,38 @@
-import warnings
 import os
 import datetime
 import threading
+import logging
 
 from flask import Flask, request, jsonify, render_template
 import joblib
 from connect_database import add_entry, fetch_all_entries
-
 from utils.url_parser import URLParser
+
+from logs.git_logger import commit_to_github
 
 app = Flask(__name__)
 
+# Set up logging
+log_file = os.path.join(
+    os.path.dirname(__file__),
+    f"logs/log_{datetime.datetime.now().strftime('%Y-%m-%d')}.txt",
+)
+logging.basicConfig(
+    filename=log_file,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+log_lock = threading.Lock()
 
 model_path = os.path.join(
     os.path.dirname(__file__), "utils/trained_models/phishing_model.pkl"
 )
 model = joblib.load(model_path)
+
+
+def safe_logging(msg):
+    with log_lock:
+        logging.info(msg)
 
 
 @app.route("/predict", methods=["POST"])
@@ -30,9 +47,9 @@ def predict():
     prediction = model.predict(parser.np_array())
 
     output = prediction[0].item()  # Convert numpy integer to Python integer
+
     # Add an entry
     result = "safe" if output == 0 else "phishing"
-
     store_thread = threading.Thread(
         target=add_entry,
         args=(
@@ -43,6 +60,11 @@ def predict():
         ),
     )
     store_thread.start()
+
+    # Log the prediction
+    safe_logging(
+        f"Prediction for {url}: {'phishing' if output == 1 else 'safe browsing'}"
+    )
 
     return jsonify(
         {
@@ -61,7 +83,7 @@ def predict():
 def fetch():
     all_entries = fetch_all_entries()
     for entry in all_entries:
-        print(entry)
+        safe_logging(f"Fetched entry: {entry}")
 
     return jsonify(all_entries)
 
@@ -87,9 +109,9 @@ def predictui():
             prediction = model.predict(parser.np_array())
 
             output = prediction[0].item()  # Convert numpy integer to Python integer
+
             # Add an entry
             result = "safe" if output == 0 else "phishing"
-            print(parser.np_array())
             store_thread = threading.Thread(
                 target=add_entry,
                 args=(
@@ -100,6 +122,12 @@ def predictui():
                 ),
             )
             store_thread.start()
+
+            # Log the prediction
+            safe_logging(
+                f"Prediction for {url}: {'phishing' if output == 1 else 'safe browsing'}"
+            )
+
             message = (
                 "Prediction says phishing URL"
                 if output == 1
@@ -107,8 +135,18 @@ def predictui():
             )
             return render_template("index.html", prediction=message, url=url)
         except Exception as e:
-            print(e)
+            safe_logging(f"An error occurred: {str(e)}")
             return render_template("index.html", prediction="broken url", url=url)
+
+
+@app.after_request
+def after_request(response):
+    print("Request processed Successfully!")
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    commit_message = f"Updating log for {date}"
+
+    commit_to_github(commit_message=commit_message)
+    return response
 
 
 if __name__ == "__main__":
